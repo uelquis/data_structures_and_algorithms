483a06402d2132d770c5027ba80b104a25b86c3b
uelquis <eman0dev@gmail.com>
2025-02-20 09:11:40 -0300

    1st commit

---
+++ src/main/java/dev/uelquis/data_structures/binary_tree/Node.java
@@ -0,0 +1,18 @@
+package dev.uelquis.data_structures.binary_tree;
+
+public final class Node <T> {
+    public final T key;
+    public final Node<T> leftNode, rightNode;
+
+    public Node(T key, Node<T> leftNode, Node<T> rightNode) {
+        this.key = key;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+
+    public Node(T key) {
+        this.key = key;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+}
