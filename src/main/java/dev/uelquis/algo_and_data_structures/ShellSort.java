c6aa3e48fa791df216d8b69323a0b7d02f88393b
uelquis <eman0dev@gmail.com>
2025-04-21 09:36:57 -0300

    implemented shell sort

---
+++ src/main/java/dev/uelquis/algo_and_data_structures/ShellSort.java
@@ -0,0 +1,45 @@
+package dev.uelquis.algo_and_data_structures;
+
+public class ShellSort {
+
+    /*
+     * Best case complexity: O(n (ln n)^2) -> when the array is already sorted.
+     * Average case complexity: O(n^1.25)
+     * Worst case complexity: o(n^2)
+     *
+     * This algorithm is a variation of Insertion Sort. It sorts elements
+     * in a specific h distance, before reducing h and sorting again recursively.
+     * It is good for medium sized files.
+     *
+     *
+     * note: Time complexity depends on how to implement the gap sequences
+     * between elements.
+     *
+     *
+     * */
+
+
+    public static double[] sort(double[] arr, SortingOrder order) {
+        double[] sorted = arr.clone();
+
+        int size = sorted.length;
+
+        // gap reduced by half every recursive call.
+        for (int gap = size/2; gap > 0; gap /= 2) {
+            for (int i = gap; i < size; ++i) {
+                double tmp = sorted[i];
+
+                boolean isAscending = order == SortingOrder.ASCENDING;
+
+                int j;
+                for (j = i; j >= gap && (sorted[j - gap] > tmp && isAscending
+                        || sorted[j - gap] < tmp && !isAscending); j -= gap)
+                    sorted[j] = sorted[j - gap];
+
+                sorted[j] = tmp;
+            }
+        }
+
+        return sorted;
+    }
+}
---
+++ src/test/java/dev/uelquis/algo_and_data_structures/ShellSortTests.java
@@ -0,0 +1,26 @@
+package dev.uelquis.algo_and_data_structures;
+
+import java.util.Arrays;
+
+import static dev.uelquis.algo_and_data_structures.Test.isSorted;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShellSortTests {
+    private final double[] array = {4,2,9,0,7,9,8.5};
+
+    @org.junit.jupiter.api.Test
+    void sortAscending() {
+        var sorted = ShellSort.sort(array, SortingOrder.ASCENDING);
+
+        assertTrue(isSorted(sorted, SortingOrder.ASCENDING));
+        System.out.println(Arrays.toString(sorted));
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortDescending() {
+        var sorted = ShellSort.sort(array, SortingOrder.DESCENDING);
+
+        assertTrue(isSorted(sorted, SortingOrder.DESCENDING));
+        System.out.println(Arrays.toString(sorted));
+    }
+}
